1b5602e9c1ecd56296aa410ded528f106bde7512
Konstantin <ostapx79gmail.com>
2025-09-25 23:30:12 +0300

    переменные в JavaScript

---
+++ firstWeekOfCourse/preparationMaterialsJSBasics/lesson5_variablesInJS/js/script.js
@@ -0,0 +1,17 @@
+// Переменные в JavaScrip
+
+// Integer
+var x = 5; // целое число
+
+// String
+var text = "Привет, это строковая переменная."; // строка
+
+// Boolean
+var logicTrue = true; // Правда, 1
+var logicFalse = false; // Ложь, 0
+
+// вывод на консоль переменные выше описаныа
+console.log(x);
+console.log(text);
+console.log(logicTrue);
+console.log(logicFalse);
